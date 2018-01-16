5216fd8697fe04728da046355c65324920b2da03
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-16 22:45:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(max) {
   return function withinRange(start, end) {
-    let dist = Math.abs(parseInt(end)-parseInt(start)
+    let dist = Math.abs(parseInt(end)-parseInt(start))
     if (dist < max) {
       return `within range by ${max - dist}`;
     }
